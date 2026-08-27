d380f50abb3265eb2d7896c6008a329276f25589
minibleyz <a32526132@gmail.com>
2026-08-27 22:58:14 +0300

    Add Theme.kt

---
+++ app/src/main/java/ru/itdo/app/ui/theme/Theme.kt
@@ -0,0 +1,16 @@
+package ru.itdo.app.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme()
+private val DarkColors = darkColorScheme()
+
+@Composable
+fun ItdoTheme(content: @Composable () -> Unit) {
+    val colors = if (isSystemInDarkTheme()) DarkColors else LightColors
+    MaterialTheme(colorScheme = colors, content = content)
+}
