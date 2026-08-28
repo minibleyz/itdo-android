74efae561468cf3b837e22972b40e9b2f3dff05b
minibleyz <a32526132@gmail.com>
2026-08-28 10:22:49 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,15 @@ dependencies {
     
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.0")
+    
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+}    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.0")
     
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
