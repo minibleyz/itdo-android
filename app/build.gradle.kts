95bfe5e0d7b48ae874a023001af5556723a3697c
minibleyz <a32526132@gmail.com>
2026-08-28 22:22:53 +0300

    fix: apply Compose Compiler plugin, drop legacy kotlinCompilerExtensionVersion
    Kotlin 2.x ships the Compose Compiler as a Gradle plugin instead of
    a manually pinned extension version. Setting
    composeOptions.kotlinCompilerExtensionVersion is ignored/conflicting
    once the plugin is applied.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -44,10 +45,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
