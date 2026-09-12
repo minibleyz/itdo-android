1199e8f883058c58224faa97d9e25867343dde2f
minibleyz <a32526132@gmail.com>
2026-09-13 00:28:14 +0300

    fix: опечатка — androidx.test.espresso, а не androidx.compose.espresso

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -121,6 +121,6 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.compose.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
